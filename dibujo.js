9cbcb9d67caae2f8e82298bacaf07d6e4ab05699
Alejandro Sanchez <alejandroingelec19@gmail.com>
2019-03-26 12:36:36 -0400

    Se modifica el plano virtual

--- dibujo.js
+++ dibujo.js
@@ -1,21 +1,33 @@
 var d = document.getElementById("dibujito"); //obtenemos el canva
 var lienzo = d.getContext("2d"); //getContext me permite tener el contexto del area
 var lineas = 30;
-var l = 0;
+var L = 0;
 var yi, xf;
 
-while(l < lineas) // tambien se puede hacer con for
-{
-	yi = 10 * l;
-	xf = 10 * (l * 1);
-	dibujarLinea("#AFF", 0, yi, xf, 300);
+while(L < lineas) // tambien se puede hacer con for
+{   
+    //plano a
+    yi = 10 * L;
+    xf = 10 * (L * 1);
+    dibujarLinea("blue", 0, yi, xf, 300);
 
-	vi = 10 * l;
-	wf = 10 * l;
+    //plano b
+    vi = 300 - (10 * L);
+    wf = 300 - (10 * L);
+    dibujarLinea("red", vi, 0, 300, wf);
+    
+    //plano c
+    ti = 10 * L;
+    uf = 300 - (10 * L)
+    dibujarLinea("green", ti, 300, 300, uf);
 
-	dibujarLinea("#AFF", vi, 0, 0, wf);
-	console.log("Linea " + 1);
-	l++;
+    //plano d
+    ai = 10 * L;
+    bf = 300 - (10 * L)
+    dibujarLinea("yellow", 0, ai, bf, 0);
+
+
+    L++;
 }
 
 
@@ -23,13 +35,13 @@ while(l < lineas) // tambien se puede hacer con for
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
 
-	//para empezar a dibujar
-	lienzo.beginPath(); // arrancamos el trazo
-	lienzo.strokeStyle = color;
-	lienzo.moveTo(xinicial, yinicial); //colocamos posicion en x, y
-	lienzo.lineTo(xfinal, yfinal); //trazamos la linea hasta x, y
-	lienzo.stroke(); // cerramos linea
-	lienzo.closePath(); // terminamos de dibujar
+    //para empezar a dibujar
+    lienzo.beginPath(); // arrancamos el trazo
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial, yinicial); //colocamos posicion en x, y
+    lienzo.lineTo(xfinal, yfinal); //trazamos la linea hasta x, y
+    lienzo.stroke(); // cerramos linea
+    lienzo.closePath(); // terminamos de dibujar
 
 
 }
